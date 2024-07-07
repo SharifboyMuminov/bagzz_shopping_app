5ef45e48ae5d2a2fe5bda8be33f75dde0cf25600
Sharifjon <sharifboymuminov@gmail.com>
2024-07-07 18:48:31 +0500

    good :)

--- lib/screens/dialog/basket_dialog.dart
+++ lib/screens/dialog/basket_dialog.dart
@@ -2,11 +2,13 @@ import 'package:bagzz_shopping/data/local/local_varibals.dart';
 import 'package:bagzz_shopping/screens/widget/dialog_item.dart';
 import 'package:bagzz_shopping/utils/app_colors.dart';
 import 'package:bagzz_shopping/utils/app_size.dart';
+import 'package:bagzz_shopping/utils/app_text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 basketDialog(BuildContext context) {
   showModalBottomSheet(
+    isScrollControlled: true,
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.only(
         topRight: Radius.circular(40.r),
@@ -17,7 +19,7 @@ basketDialog(BuildContext context) {
     backgroundColor: AppColors.white.withOpacity(0.9),
     context: context,
     builder: (BuildContext context) {
-      return Padding(
+      return SingleChildScrollView(
         padding: EdgeInsets.symmetric(
           horizontal: 22.we,
           vertical: 10.he,
@@ -46,6 +48,28 @@ basketDialog(BuildContext context) {
               productModel: products[4],
               onTab: () {},
             ),
+            44.getH(),
+            TextButton(
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 24.we,
+                  vertical: 12.he,
+                ),
+                backgroundColor: AppColors.black,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero,
+                ),
+              ),
+              onPressed: () {},
+              child: Text(
+                "Add all to cart".toUpperCase(),
+                style: AppTextStyle.workSansMedium.copyWith(
+                  fontSize: 16.sp,
+                  color: AppColors.white,
+                ),
+              ),
+            ),
+            20.getH(),
           ],
         ),
       );
