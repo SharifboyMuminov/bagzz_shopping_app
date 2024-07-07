acbfe092183209157449d9b3360737f9046c15dd
Sharifjon <sharifboymuminov@gmail.com>
2024-07-07 07:31:20 +0500

    first commit

---
+++ android/app/src/main/kotlin/bagzz/uz/bagzz_shopping/MainActivity.kt
@@ -0,0 +1,5 @@
+package bagzz.uz.bagzz_shopping
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/app_text_style.dart
@@ -0,0 +1,84 @@
+import 'package:bagzz_shopping/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  // PlayfairDisplay
+  static TextStyle playfairDisplayThin = const TextStyle(
+    fontFamily: "PlayfairDisplay",
+    fontWeight: FontWeight.w200,
+    color: AppColors.black,
+  );
+  static TextStyle playfairDisplayLight = const TextStyle(
+    fontFamily: "PlayfairDisplay",
+    fontWeight: FontWeight.w300,
+    color: AppColors.black,
+  );
+  static TextStyle playfairDisplayRegular = const TextStyle(
+    fontFamily: "PlayfairDisplay",
+    fontWeight: FontWeight.w400,
+    color: AppColors.black,
+  );
+  static TextStyle playfairDisplayMedium = const TextStyle(
+    fontFamily: "PlayfairDisplay",
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+  );
+
+  static TextStyle playfairDisplaySemiBold = const TextStyle(
+    fontFamily: "PlayfairDisplay",
+    fontWeight: FontWeight.w600,
+    color: AppColors.black,
+  );
+
+  static TextStyle playfairDisplayBold = const TextStyle(
+    fontFamily: "PlayfairDisplay",
+    fontWeight: FontWeight.w700,
+    color: AppColors.black,
+  );
+
+  static TextStyle playfairDisplayBlack = const TextStyle(
+    fontFamily: "PlayfairDisplay",
+    fontWeight: FontWeight.w800,
+    color: AppColors.black,
+  );
+
+  // WorkSans
+  static TextStyle workSansThin = const TextStyle(
+    fontFamily: "WorkSans",
+    fontWeight: FontWeight.w200,
+    color: AppColors.black,
+  );
+  static TextStyle workSansLight = const TextStyle(
+    fontFamily: "WorkSans",
+    fontWeight: FontWeight.w300,
+    color: AppColors.black,
+  );
+  static TextStyle workSansRegular = const TextStyle(
+    fontFamily: "WorkSans",
+    fontWeight: FontWeight.w400,
+    color: AppColors.black,
+  );
+  static TextStyle workSansMedium = const TextStyle(
+    fontFamily: "WorkSans",
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+  );
+
+  static TextStyle workSansSemiBold = const TextStyle(
+    fontFamily: "WorkSans",
+    fontWeight: FontWeight.w600,
+    color: AppColors.black,
+  );
+
+  static TextStyle workSansBold = const TextStyle(
+    fontFamily: "WorkSans",
+    fontWeight: FontWeight.w700,
+    color: AppColors.black,
+  );
+
+  static TextStyle workSansBlack = const TextStyle(
+    fontFamily: "WorkSans",
+    fontWeight: FontWeight.w800,
+    color: AppColors.black,
+  );
+}
