37c5618ca6a219ea74822c61f52f0b3cb95c1f15
Sharifjon <sharifboymuminov@gmail.com>
2024-07-07 17:42:57 +0500

    good :)

--- lib/screens/tab_box.dart
+++ lib/screens/tab_box.dart
@@ -1,6 +1,9 @@
 import 'package:bagzz_shopping/screens/home/home_screen.dart';
+import 'package:bagzz_shopping/utils/app_colors.dart';
 import 'package:bagzz_shopping/utils/app_size.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class TabBox extends StatefulWidget {
   const TabBox({super.key});
@@ -22,9 +25,69 @@ class _TabBoxState extends State<TabBox> {
     height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: IndexedStack(
-        index: activePage,
-        children: _screens,
+      body: Stack(
+        children: [
+          IndexedStack(
+            index: activePage,
+            children: _screens,
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              bottom: 20.he,
+              left: 12.we,
+              right: 12.we,
+            ),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: AppColors.white,
+                  borderRadius: BorderRadius.circular(69.r),
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColors.black.withOpacity(0.14),
+                      blurRadius: 56,
+                      spreadRadius: 0,
+                      offset: const Offset(0, 14),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 12.he),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          "assets/icons/home.svg",
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          "assets/icons/search.svg",
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          "assets/icons/favorite.svg",
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          "assets/icons/basket.svg",
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
